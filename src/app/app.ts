3011f3cca53a7bf6d9281ebdec6150705043465b
Ana Vicovac <anavicovac@gmail.com>
2026-03-16 21:31:16 +0100

    game background init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameWrapper } from './game-wrapper/game-wrapper';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameWrapper],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('search-algorithms-visualization');
---
+++ src/app/game-wrapper/game-wrapper.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-wrapper',
+  imports: [],
+  templateUrl: './game-wrapper.html',
+  styleUrl: './game-wrapper.scss',
+})
+export class GameWrapper {}
